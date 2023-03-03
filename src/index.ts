e323f19558cf7b1f06cecffdfeb7a05683b65456
Daniel Burger <public@danielburger.online>
2023-03-03 14:42:36 +0100

    Add WebSocket logic to Worker

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-export default {
-  async fetch(_request: Request, _env: any): Promise<Response> {
-    return new Response('Hello world');
-  },
-};
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request: Request) {
+  const upgradeHeader = request.headers.get('Upgrade');
+  if (!upgradeHeader || upgradeHeader !== 'websocket') {
+    return new Response('Expected Upgrade: websocket', { status: 426 });
+  }
+
+  const webSocketPair = new WebSocketPair();
+  const [client, server] = Object.values(webSocketPair);
+
+  server.accept();
+  server.addEventListener('message', (event) => {
+    console.log(event);
+  });
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client,
+  });
+}
